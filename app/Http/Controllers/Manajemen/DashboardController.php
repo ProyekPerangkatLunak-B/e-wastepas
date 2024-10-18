f5aa6f9d0284ad6d404231b0ff93f4b71a24d671
rivaannn <mrivans2002@gmail.com>
2024-10-18 16:06:43 +0700

    Inisialisasi Laravel + TailwindCSS

---
+++ app/Http/Controllers/Manajemen/DashboardController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Manajemen;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Masyrakat/RegistrasiController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Masyrakat;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RegistrasiController extends Controller
+{
+    //
+}
