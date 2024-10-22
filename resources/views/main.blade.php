ab89b22f3bd58e1957917ecac5148a3dc409cee6
rivaannn <mrivans2002@gmail.com>
2024-10-22 18:29:10 +0700

    Recycleme-frontend: Mengerjakan Prototype 1 Modul 1 Sub Modul 2

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,32 +1,35 @@
 <!doctype html>
-<html>
+<html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     {{-- Link Fav Icon --}}
-    <link rel="icon" href="{{ asset('#') }}" type="image/x-icon" />
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon" />
 
-    {{-- Link Font Google  --}}
+    {{-- Link Font Google --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@200;400;600;800&display=swap" rel="stylesheet">
 
     {{-- Link Load CSS --}}
-    @vite(['resources/css/app.css', 'resources/js/app.js'])`
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    {{-- Alphine Js --}}
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-
-    <title>E-Wastepas</title>
+    <title>E-WastePas</title>
 </head>
 
-<body class="">
+<body>
 
-    {{-- Components --}}
+    {{-- Header --}}
+    @include('assets.partials.header')
 
-    @yield('content')
-
+    {{-- Sidebar --}}
+    @include('assets.partials.sidebar')
+    
+    {{-- Main Content --}}
+    <div class="pt-6 pl-[20rem]">
+        @yield('content')
+    </div>    
+    
 </body>
-
 </html>
