a44687dc34d32a02bf3a6e9797ec66cf455116c1
vinaanurfauziah <vinaanurfauziah@gmail.com>
2024-10-25 21:54:35 +0700

    Shorekeeper-frontend: Mengerjakan Prototype 1 Modul 2 Sub Modul 1 - UC_B03_1

---
+++ resources/views/components/mitra-kurir/auth/input.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <label for="{{ $id }}" class="block text-sm font-medium leading-6 text-gray-900">{{ $label }}</label>
+    <div class="mt-2">
+        <input id="{{ $id }}" name="{{ $name }}" type="{{ $type }}" placeholder="{{ $placeholder }}" required class="block w-full rounded-lg border border-second-color py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 bg-light-gray sm:text-sm sm:leading-6 pl-3">
+    </div>
+</div>
