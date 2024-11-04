3a0b7bafa68608e26245f60074d7968eebd08638
adiyatan <adiyazam03@gmail.com>
2024-11-04 17:44:51 +0700

    Add KategoriSampah model and admin controller; update migrations and views

---
+++ app/Models/KategoriSampah.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KategoriSampah extends Model
+{
+    use HasFactory;
+
+    protected $table = 'kategori_sampah';
+    protected $primaryKey = 'id_kategori_sampah';
+
+    protected $fillable = [
+        'nama_kategori_sampah',
+        'deskripsi_kategori_sampah',
+    ];
+}
