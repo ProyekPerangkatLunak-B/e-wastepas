89380a920454c2173aff68c267056f4e8e2fcdc4
rivaannn <mrivans2002@gmail.com>
2024-11-05 20:18:26 +0700

    Recycleme-frontend: menambahkan halaman utama, memperbaiki skenario 1 dan 2

--- resources/views/masyarakat/penjemputan-sampah/detail-riwayat.blade.php
+++ resources/views/masyarakat/penjemputan-sampah/detail-riwayat.blade.php
@@ -7,7 +7,7 @@
     <div class="flex" aria-label="Breadcrumb">
         <ol class="inline-flex items-center -ml-3 space-x-1 md:space-x-3">
           <li class="inline-flex items-center">
-            <a href="#" class="inline-flex ml-1 text-sm font-medium text-gray-800 hover:underline md:ml-2">
+            <a href="{{ route('masyarakat.penjemputan.total-riwayat') }}" class="inline-flex ml-1 text-sm font-medium text-gray-800 hover:underline md:ml-2">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4 mr-4">
                 <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
