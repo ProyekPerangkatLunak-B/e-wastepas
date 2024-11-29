780f0f14ba7b3d35c3cc008d12fd9904fc3c59d0
ikmalpra97 <ilhamaakmalrp@gmail.com>
2024-11-30 02:44:20 +0700

    Menambah jalur backend pada permintaan penjemputan beserta tampilan data = database
    # Conflicts:
    #	resources/views/mitra-kurir/penjemputan-sampah/detail-kategori.blade.php
    #	resources/views/mitra-kurir/penjemputan-sampah/kategori.blade.php

--- app/Http/Controllers/MitraKurir/PenjemputanSampahMitraKurirController.php
+++ app/Http/Controllers/MitraKurir/PenjemputanSampahMitraKurirController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\MitraKurir;
 
 use App\Http\Controllers\Controller;
+use App\Models\Dropbox;
+use App\Models\Jenis;
+use App\Models\Kategori;
+use App\Models\Pelacakan;
+use App\Models\Pengguna;
+use App\Models\Penjemputan;
+use App\Models\SampahDetail;
 use Illuminate\Http\Request;
 use App\Models\JenisSampah;
 use App\Models\KategoriSampah;
+use Illuminate\Support\Facades\DB;
 
 class PenjemputanSampahMitraKurirController extends Controller
 {
@@ -13,21 +21,46 @@ class PenjemputanSampahMitraKurirController extends Controller
     //untuk mengambil data dari database tabel kategori_sampah
     public function kategori()
     {
-        $kategori = KategoriSampah::all();
+        $kategori = Kategori::all();
         return view(('mitra-kurir.penjemputan-sampah.kategori'), compact('kategori'));
     }
 
     //untuk mengambil data dari database tabel jenis_sampah
     public function detailKategori($id)
     {
-        $jenis = JenisSampah::where('id_kategori_sampah', $id)->paginate(6);
-        $kategori = KategoriSampah::find($id);
+        $jenis = Jenis::where('id_kategori', $id)->paginate(6);
+        $kategori = Kategori::find($id);
         return view('mitra-kurir.penjemputan-sampah.detail-kategori', compact('jenis', 'kategori'));
     }
-    
+
     public function permintaan()
     {
-        return view('mitra-kurir.penjemputan-sampah.permintaan-penjemputan');
+        $data = DB::table('penjemputan')
+            ->join('pelacakan', 'penjemputan.id_pengguna_masyarakat', '=', 'pelacakan.id_pelacakan')
+            ->join('pengguna', 'penjemputan.id_pengguna_masyarakat', '=', 'pengguna.id_pengguna')
+            ->join('sampah_detail', 'penjemputan.id_pengguna_masyarakat', '=', 'sampah_detail.id_penjemputan')
+            ->join('jenis', 'jenis.id_jenis', '=', 'sampah_detail.id_jenis')
+            ->where('pelacakan.status', 'Menunggu Konfirmasi')
+            ->select('pengguna.nama', 'pelacakan.status','pelacakan.id_pelacakan','jenis.nama_jenis','sampah_detail.berat','penjemputan.id_penjemputan')
+            ->get();
+
+        $pengguna = Pengguna::all();
+        $jenis = Jenis::all();
+        $sampah = SampahDetail::all();
+        $pelacakan = Pelacakan::where('status', 'Menunggu Konfirmasi')->get();
+        $penjemputan = Penjemputan::all();
+
+        return view('mitra-kurir.penjemputan-sampah.permintaan-penjemputan', compact('data'));
+    }
+
+    public function detailPermintaan($id)
+    {
+        $pengguna = Pengguna::find($id);
+        $jenis = Jenis::all();
+        $sampah = SampahDetail::all();
+        $dropbox = Dropbox::all();
+        $penjemputan = Penjemputan::all();
+        return view('mitra-kurir.penjemputan-sampah.detail-permintaan', compact('pengguna','jenis','sampah','dropbox','penjemputan'));
     }
 
     public function dropbox()
--- resources/views/mitra-kurir/penjemputan-sampah/kategori.blade.php
+++ resources/views/mitra-kurir/penjemputan-sampah/kategori.blade.php
@@ -35,27 +35,24 @@
 
             {{-- Card Section --}}
             <div class="flex justify-center mt-4">
-                <div class="grid grid-cols-1 gap-4 px-12 mt-4 lg:grid-cols-3 lg:gap-4">
-                    @if (count($kategori) == 0)
-                        <div class="flex justify-center ml-[500px] mt-56 items-center col-span-full bg-white-normal w-[400px] h-[300px] rounded-xl shadow-lg">
+                <div class="grid grid-cols-3 gap-6 mx-auto">
+                @if (count($kategori) == 0)
+                        <div
+                            class="flex justify-center mt-56 items-center col-span-full bg-white-normal w-[400px] h-[300px] rounded-xl shadow-lg">
                             <div class="text-center">
-                                <img src="{{ asset('img/masyarakat/penjemputan-sampah/x-circle 3.png') }}" alt="Tidak Ditemukan" class="w-[100px] h-[100px] mx-auto mb-4">
                                 <p class="text-lg font-semibold text-gray-500">Kategori {{ $search ?? 'Sampah Elektronik' }} tidak ditemukan.</p>
                             </div>
                         </div>
-                    @else
+                @else
                     @foreach ($kategori as $kategoriSampah)
-                        <x-card 
-                            title="{{ $kategoriSampah->nama_kategori_sampah }}"
-                            description="{{ $kategoriSampah->deskripsi_kategori_sampah }}"
-                            image="https://picsum.photos/1280/720" 
-                            link="{{ route('mitra-kurir.penjemputan.detail-kategori', $kategoriSampah->id_kategori_sampah) }}" 
-                        />
+                            <x-card
+                                title="{{ $kategoriSampah->nama_kategori }}"
+                                description="{{ $kategoriSampah->deskripsi_kategori }}"
+                                image="https://picsum.photos/1280/720"
+                                link="{{ route('mitra-kurir.penjemputan.detail-kategori', $kategoriSampah->id_kategori) }}"
+                            />
                     @endforeach
-                    @endif
-                </div>
-            </div>
-
+                @endif
         </div>
     </div>
 @endsection
