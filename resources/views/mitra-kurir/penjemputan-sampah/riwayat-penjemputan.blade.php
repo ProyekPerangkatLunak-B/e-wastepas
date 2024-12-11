3edae7a2c3b9236e3ad1b9edb8c164a72672e72f
ikmalpra97 <ilhamaakmalrp@gmail.com>
2024-12-12 02:57:34 +0700

    membuat backend halaman riwayat

--- resources/views/mitra-kurir/penjemputan-sampah/riwayat-penjemputan.blade.php
+++ resources/views/mitra-kurir/penjemputan-sampah/riwayat-penjemputan.blade.php
@@ -6,18 +6,23 @@
             <h2 class="text-xl font-semibold leading-relaxed ml-14">Total Sampah</h2>
             <h4 class="text-base font-normal ml-14 mb-8">Semua total sampah yang telah anda jemput</h4>
             <div class="flex space-x-8 px-12 mb-8">
+                @php
+                    $totalSampah = $data->count();
+                    $totalBeratSampah = $data->sum('berat');
+                @endphp
                 <!-- Total Sampah -->
                 <div class="bg-white-200 rounded-2xl shadow-md p-6 flex items-center space-x-4 w-1/3">
-                    <div class="text-5xl font-bold text-green-600">28<span class="text-lg text-gray-900">pcs</span></div>
+                    <div class="text-5xl font-bold text-green-600">{{ $totalSampah }}<span class="text-lg text-gray-900">pcs</span></div>
                     <div class="flex flex-col">
+
                         <h3 class="text-lg font-semibold text-center">Total Sampah</h3>
                         <p class="text-sm text-gray-500 text-center">Total sampah yang sudah dijemput</p>
                     </div>
                 </div>
-            
+
                 <!-- Total Berat Sampah -->
                 <div class="bg-white-200 rounded-2xl shadow-md p-6 flex items-center space-x-4 w-1/3">
-                    <div class="text-5xl font-bold text-green-600">150<span class="text-lg text-gray-900">Kg</span></div>
+                    <div class="text-5xl font-bold text-green-600">{{ $totalBeratSampah }}<span class="text-lg text-gray-900">Kg</span></div>
                     <div class="flex flex-col">
                         <h3 class="text-lg font-semibold text-center">Total Berat Sampah</h3>
                         <p class="text-sm text-gray-500 text-center">Total berat sampah yang sudah dijemput</p>
@@ -64,54 +69,31 @@
                 </div>
             </div>
 
-            <div class="grid grid-cols-1 gap-4 px-12 mt-4 lg:grid-cols-3 lg:gap-4">
-                {{-- Card Section --}}
-                <div class="relative w-full h-auto max-w-sm bg-white-100 border border-gray-200 rounded-2xl shadow-md hover:shadow-lg">
-                    {{-- berat --}}
-                    <div class="absolute top-0 left-0 px-6 py-2 bg-secondary-normal text-white-100 font-semibold rounded-br-2xl rounded-tl-2xl">
-                        31 Kg
-                    </div>
-                    <div class="flex flex-col items-center pb-5">
-                        <div class="h-20 rounded-full overflow-hidden bg-white mt-5">
-                            <img class="object-cover w-full h-full" src="https://picsum.photos/700/700" alt="Rahma"/>
-                        </div>
-                        <div class="flex flex-col items-center p-4 text-center">
-                            <h3 class="text-base font-semibold text-gray-900">Rahma</h3>
-                        </div>
-                        <div>
-                            <ul class="my-3 space-y-3">
-                                {{-- item --}}
-                                <li>
-                                    <a href="#" class="w-64 flex items-center p-3 text-base font-bold text-gray-900 rounded-2xl bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-slate-200 dark:hover:bg-gray-500 dark:text-white">
-                                    <div class="flex-shrink-0">
-                                        <img class="w-8 h-8 rounded-full" src="https://picsum.photos/700/700" alt="2 Pcs">
-                                    </div>
-                                    <span class="flex-1 ms-3 whitespace-nowrap text-lg font-normal">Laptop</span>
-                                    <span class="inline-flex items-center justify-center px-2 py-0.5 ms-3 text-base font-medium text-lime-900  ">2 Pcs</span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#" class="w-64 flex items-center p-3 text-base font-bold text-gray-900 rounded-2xl bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-slate-200 dark:hover:bg-gray-500 dark:text-white">
-                                    <div class="flex-shrink-0">
-                                        <img class="w-8 h-8 rounded-full" src="https://picsum.photos/700/700" alt="2 Pcs">
-                                    </div>
-                                    <span class="flex-1 ms-3 whitespace-nowrap text-lg font-normal">Laptop</span>
-                                    <span class="inline-flex items-center justify-center px-2 py-0.5 ms-3 text-base font-medium text-lime-900  ">2 Pcs</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    
-                    <!-- Button Detail -->
-                    <div class="flex justify-center mt-2 mb-4">
-                        <a href="{{ route('mitra-kurir.penjemputan.detail-riwayat') }}" type="button" class="px-9 py-2 font-semibold text-white-100 bg-gradient-to-b from-secondary-normal to-primary-normal rounded-full shadow-md">
-                            Detail
-                        </a>
+            @if (count($data) == 0)
+                <div
+                    class="flex justify-center mt-56 items-center col-span-full bg-white-normal w-[400px] h-[300px] rounded-xl shadow-lg">
+                    <div class="text-center">
+                        <p class="text-lg font-semibold text-gray-500">Pengguna {{ $search ?? 'pengguna' }} tidak ditemukan.</p>
                     </div>
                 </div>
-
-            </div>
+            @else
+{{--                 Card Section --}}
+                @php
+                    $groupData = $data->groupBy('nama');
+                @endphp
+                <div class="grid grid-cols-1 gap-4 px-12 mt-4 lg:grid-cols-3 lg:gap-4">
+                    @foreach ($groupData as $name => $items)
+                        <x-card-riwayat
+                        name="{{ $name }}"
+                        status="{{ $items->first()->status }}"
+                        image="https://picsum.photos/700/700"
+                        imageItem="https://picsum.photos/700/700"
+                        :items="$items"
+                        link="{{ route('mitra-kurir.penjemputan.detail-riwayat', $items->first()->id_penjemputan) }}"
+                        />
+                        @endforeach
+                    </div>
+            @endif
         </div>
     </div>
 @endsection
