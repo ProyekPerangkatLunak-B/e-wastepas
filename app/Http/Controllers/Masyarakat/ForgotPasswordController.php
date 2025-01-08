207adac42db22c7361e87bce7c8ab1ecaa09806c
Adiya Tanaya Permana <80138309+adiyatan@users.noreply.github.com>
2025-01-08 20:26:47 +0700

    Revert "pioneer-frontend: menambahkan alert/skenario alternatif di halaman login, register, dan forgot-password"

--- app/Http/Controllers/Masyarakat/ForgotPasswordController.php
+++ app/Http/Controllers/Masyarakat/ForgotPasswordController.php
@@ -11,7 +11,6 @@ use App\Models\User;
 
 class ForgotPasswordController extends Controller
 {
-
     public function sendResetLinkEmail(Request $request)
     {
         // Validasi input email
@@ -32,17 +31,17 @@ class ForgotPasswordController extends Controller
             $resetUrl = url('password/reset/' . $token);
 
             // Kirim email reset password
-
+            
             Mail::send([], [], function ($message) use ($user, $resetUrl) {
                 $message->to($user->email)
                         ->subject('Reset Password')
-                        ->html('Click the following link to reset your password: <a href="' . $resetUrl . '">Reset Password</a>');
+                        ->html('Click the following link to reset your password: <a href="' . $resetUrl . '">Reset Password</a>'); 
             });
             // Berikan respons sukses
             return back()->with(['status' => 'Link reset password telah dikirim ke email Anda.']);
         }
 
         // Jika email tidak ditemukan
-        return back()->withErrors(['email' => 'Email tidak terdaftar']);
+        return back()->withErrors(['email' => 'Email tidak ditemukan dalam sistem kami.']);
     }
 }
--- resources/views/masyarakat/registrasi/register.blade.php
+++ resources/views/masyarakat/registrasi/register.blade.php
@@ -13,55 +13,58 @@
 
             <div class="w-full flex-20 mt-2">
                 <div class="flex flex-col flex justify-around items-center">
-
             </div>
+
+             @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li class="text-red-500 p-3 rounded-lg">{{ $error }}</li>
+                @endforeach
+            </ul>
+            @endif
 
             <!-- Form Registrasi -->
                 <div class="mx-auto max-w-md">
                 <form action="{{ route('masyarakat.register.submit') }}" method="POST">
                 @csrf
                   <div>
+                      @if ($errors->any())
+                        <div>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
                     <label for="name" class="block mt-4 text-md font-medium leading-9 text-gray-500">Nama</label>
                         <input
                             class="w-full mt-2 px-4 py-2 rounded-lg font-medium bg-gray-100 border border-gray-300 text-md focus:outline-none focus:border-green-800 focus:bg-white"
-                            type="name" required name="name" placeholder="Masukkan Nama" value="{{ old('name') }}"/>
-                  </div>
+                            type="name" required name="name" placeholder="Masukkan Nama"/>
+                    </div>
                   <div>
-                    <label for="nomor_telepon" class="block mt-4 text-md font-medium leading-9 text-gray-500">No. Telepon</label>
+                    <label for="telepon" class="block mt-4 text-md font-medium leading-9 text-gray-500">No. Telepon</label>
                         <input
-                            class="w-full mt-2 px-4 py-2 rounded-lg font-medium bg-gray-100 border border-gray-300 text-md focus:outline-none focus:border-green-800 focus:bg-white @error('nomor_telepon') border-red-500 @enderror"
-                            type="nomor_telepon" required name="nomor_telepon" placeholder="Masukkan No. Telepon" value="{{ old('nomor_telepon') }}"/>
-                    @error('nomor_telepon')
-                        <p class="text-red-500 text-sm mt-2">{{ $message }}</p>
-                    @enderror
-                  </div>
+                            class="w-full mt-2 px-4 py-2 rounded-lg font-medium bg-gray-100 border border-gray-300 text-md focus:outline-none focus:border-green-800 focus:bg-white"
+                            type="tel" required name="tel" placeholder="Masukkan No. Telepon"/>
+                    </div>
                   <div>
-                    <label for="nomor_ktp" class="block mt-4 text-md font-medium leading-9 text-gray-500">No. KTP</label>
+                    <label for="no-ktp" class="block mt-4 text-md font-medium leading-9 text-gray-500">No. KTP</label>
                         <input
-                            class="w-full mt-2 px-4 py-2 rounded-lg font-medium bg-gray-100 border border-gray-300 text-md focus:outline-none focus:border-green-800 focus:bg-white @error('nomor_ktp') border-red-500 @enderror"
-                            type="nomor_ktp" required name="nomor_ktp" placeholder="Masukkan No. KTP" value="{{ old('nomor_ktp') }}"/>
-                    @error('nomor_ktp')
-                        <p class="text-red-500 text-sm mt-2">{{ $message }}</p>
-                    @enderror
-                  </div>
+                            class="w-full mt-2 px-4 py-2 rounded-lg font-medium bg-gray-100 border border-gray-300 text-md focus:outline-none focus:border-green-800 focus:bg-white"
+                            type="text" required name="no-rek" placeholder="Masukkan No. KTP"/>
+                    </div>
                   <div>
                     <label for="email" class="block mt-4 text-md font-medium leading-9 text-gray-500">Email</label>
                         <input
-                            class="w-full mt-2 px-4 py-2 rounded-lg font-medium bg-gray-100 border border-gray-300 text-md focus:outline-none focus:border-green-800 focus:bg-white @error('email') border-red-500 @enderror"
-                            type="email" required name="email" placeholder="Masukkan Email" value="{{ old('email') }}"/>
-                    @error('email')
-                        <p class="text-red-500 text-sm mt-2">{{ $message }}</p>
-                    @enderror
-                  </div>
+                            class="w-full mt-2 px-4 py-2 rounded-lg font-medium bg-gray-100 border border-gray-300 text-md focus:outline-none focus:border-green-800 focus:bg-white"
+                            type="email" required name="email" placeholder="Masukkan Email"/>
+                    </div>
                     <div>
                     <label for="password" class="block mt-4 text-md font-medium leading-9 text-gray-500">Password</label>
                         <input
-                            id="password" class="w-full mt-2 px-4 py-2 rounded-lg font-medium bg-gray-100 border border-gray-300 text-md focus:outline-none focus:border-green-800 focus:bg-white @error('password') border-red-500 @enderror"
-                            type="password" required name="password" placeholder="Masukkan Password" />
-                            <span id="password-alert" class="error"></span>
-                    @error('password')
-                        <p class="text-red-500 text-sm mt-2">{{ $message }}</p>
-                    @enderror
+                            class="w-full mt-2 px-4 py-2 rounded-lg font-medium bg-gray-100 border border-gray-300 text-md focus:outline-none focus:border-green-800 focus:bg-white"
+                            type="password" required name="password" placeholder="Masukkan Password"/>
                     </div>
                       <div class="">
                 </div>
@@ -71,38 +74,6 @@
                       class="focus:outline-none text-slate-50 bg-gradient-to-r from-lime-500 to-green-600 hover:bg-gradient-to-l w-full py-3 font-bold rounded-lg mt-8 text-base"> Masuk
                   </button>
                 </form>
-
-                <style>
-                    .error {
-                        color: red;
-                        font-size: 14px;
-                    }
-                    .success {
-                        color: green;
-                        font-size: 14px;
-                    }
-                </style>
-
-                <script>
-                    const passwordInput = document.getElementById('password');
-                    const alertBox = document.getElementById('password-alert');
-
-                    passwordInput.addEventListener('input', () => {
-                        const password = passwordInput.value;
-
-                        // Cek panjang password
-                        if (password.length < 10) {
-                            alertBox.textContent = 'Password Anda terlalu lemah. Minimal 10 karakter.';
-                            alertBox.classList.remove('success');
-                            alertBox.classList.add('error');
-                        } else {
-                            // Password kuat
-                            alertBox.textContent = 'Password Anda sudah kuat.';
-                            alertBox.classList.remove('error');
-                            alertBox.classList.add('success');
-                        }
-                    });
-                </script>
                 </a>
                   <p class="mt-2 text-center text-sm text-gray-500">Sudah punya akun?
                   <a href="./login" class="font-semibold leading-6 text-gray-700 hover:text-gray-500">Masuk disini.</a></p>
