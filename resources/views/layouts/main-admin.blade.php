c91babd522ba53c622e80b44f36d0a8c9ed6f298
adiyatan <adiyazam03@gmail.com>
2025-01-14 21:37:58 +0700

    Add CSRF token meta tag to main-admin layout for enhanced security

--- resources/views/layouts/main-admin.blade.php
+++ resources/views/layouts/main-admin.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     {{-- Link Fav Icon --}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon" />
 
     {{-- Link Font Google --}}
